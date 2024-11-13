c3591acf8f493547134064d276ab27475d6a4fea
suplongv <nguvt8869@ut.edu.vn>
2024-11-13 11:19:41 +0700

    them lieu thu nhap, chi phi

--- baitap.cpp
+++ baitap.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main(){
     int a,b;
-    cin >> a>> b;
+    float soDuTrongTaiKhoan = 0.0f;
+
+    cout >> "Nhap lieu thu nhap : ";
+    cin >> a;
+
+    cout >> "Nhap lieu chi phi : ";
+    cin >> b;
     return 0;
 }
